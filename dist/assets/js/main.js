1b39cb5a3adf70610815bc8dfbf475550d1649d2
Rostyslav Haluzinskiy <106879174+RossHaluz@users.noreply.github.com>
2023-07-10 21:53:07 +0300

    add

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -2,9 +2,16 @@
 const onOpenMobileMenu = document.querySelector(".mobile-menu-btn");
 const onCloseMobileMenu = document.querySelector('.mobile-menu-btn-close');
 const mobileMenu = document.querySelector('.menu-overlay');
+const emailForm = document.getElementById('#emailForm');
+const formBrn = document.querySelector('.sign-up-desc__form--btn')
 
 onOpenMobileMenu.addEventListener("click", openModal)
 onCloseMobileMenu.addEventListener("click", closeModal)
+formBrn.addEventListener('click', onSubmitForm);
+
+function onSubmitForm(e){
+  e.preventDefault()
+}
 
 function openModal() {
     onOpenMobileMenu.classList.add('is-open');
@@ -19,3 +26,19 @@ function openModal() {
     mobileMenu.classList.add('is-open')
     document.body.classList.remove('no-scroll')
   }
+
+  $(emailForm).validate({
+    rules: {
+      email: {
+        required: 'Emaile is required!',
+        email: true
+      }
+    },
+
+    submitHandler: function(form) {
+      // some other code
+      // maybe disabling submit button
+      // then:
+      $(form).submit();
+    }
+  })
